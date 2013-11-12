4321c847696b46f16724b74428f7d2cdf73fdc6c
Rob Szumlakowski <robsz@pivotallabs.com>
2013-11-12 09:00:14 -0600

    @xtreme-rob-szumlakowski - removed roboguice since it was causing silly warning at runtime that looked like crashes

--- PushLib-Sample/src/main/java/com/gopivotal/pushlib/MainActivity.java
+++ PushLib-Sample/src/main/java/com/gopivotal/pushlib/MainActivity.java
@@ -1,16 +1,13 @@
 package com.gopivotal.pushlib;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
-import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
-
-public class MainActivity extends RoboActivity {
+public class MainActivity extends ActionBarActivity {
 
     private static final String SENDER_ID = "816486687340";
 
-    @InjectView(R.id.outputText)
     TextView outputText;
 
     private PushLib pushLib;
@@ -19,6 +16,8 @@ public class MainActivity extends RoboActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputText = (TextView) findViewById(R.id.outputText);
         outputText.setText("Ready\n");
 
         pushLib = PushLib.init(this, SENDER_ID);
