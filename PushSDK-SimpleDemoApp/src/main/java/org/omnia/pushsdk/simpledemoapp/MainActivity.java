a3792c156e7242fa077db771349dd79d38e3116f
Rob Szumlakowski <robsz@pivotallabs.com>
2014-04-04 10:58:33 -0500

    @pivotal-rob-szumlakowski - updated Simple Demo App to use new variant on the new staging server [#68886510]

--- PushSDK-SimpleDemoApp/src/main/java/org/omnia/pushsdk/simpledemoapp/MainActivity.java
+++ PushSDK-SimpleDemoApp/src/main/java/org/omnia/pushsdk/simpledemoapp/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.omnia.pushsdk.PushLib;
@@ -17,11 +15,11 @@ public class MainActivity extends ActionBarActivity {
     // Set to your "Project Number" on your Google Cloud project
     private static final String GCM_SENDER_ID = "961895792376";
 
-    // Set to your "Release UUID", as provided by the Omnia console
-    private static final String RELEASE_UUID = "8a72b0a3-148d-4b3f-b47b-b3a07dd536ca";
+    // Set to your "Variant UUID", as provided by the Omnia console
+    private static final String VARIANT_UUID = "9a0bbe07-eb1e-483d-9517-ff9647de0fcc";
 
-    // Set to your "Release Secret" as provided by the Omnia console
-    private static final String RELEASE_SECRET = "d6928667-0192-4c10-b642-744d9671d290";
+    // Set to your "Variant Secret" as provided by the Omnia console
+    private static final String VARIANT_SECRET = "fbd90567-078d-4f3f-98bd-33e018cbb1a8";
 
     // Set to your own defined alias for this device.  May not be null.  May be empty.
     private static final String DEVICE_ALIAS = "test_device_alias";
@@ -48,7 +46,7 @@ public class MainActivity extends ActionBarActivity {
     private void registerForPushNotifications() {
 
         // Create the parameters object
-        final RegistrationParameters parameters = new RegistrationParameters(GCM_SENDER_ID, RELEASE_UUID, RELEASE_SECRET, DEVICE_ALIAS);
+        final RegistrationParameters parameters = new RegistrationParameters(GCM_SENDER_ID, VARIANT_UUID, VARIANT_SECRET, DEVICE_ALIAS);
         try {
 
             // Register for push notifications.  The listener itself is optional (may be null).
