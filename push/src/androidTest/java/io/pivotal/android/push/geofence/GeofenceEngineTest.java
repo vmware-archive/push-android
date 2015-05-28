8cac24de880a2a8b7cc11c4a48cb7624bea3ff81
Rob Szumlakowski and Anisha Hirji <ahirji@pivotal.io>
2015-05-28 11:49:42 -0400

    RobS/AnishaH [#94185812] Renamed test son file to align with iOS.

--- push/src/androidTest/java/io/pivotal/android/push/geofence/GeofenceEngineTest.java
+++ push/src/androidTest/java/io/pivotal/android/push/geofence/GeofenceEngineTest.java
@@ -2,13 +2,10 @@ package io.pivotal.android.push.geofence;
 
 import android.test.AndroidTestCase;
 
-import com.google.gson.reflect.TypeToken;
-
 import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
 
-import io.pivotal.android.push.model.geofence.PCFPushGeofenceData;
 import io.pivotal.android.push.model.geofence.PCFPushGeofenceDataList;
 import io.pivotal.android.push.model.geofence.PCFPushGeofenceLocationMap;
 import io.pivotal.android.push.model.geofence.PCFPushGeofenceResponseData;
@@ -600,9 +597,7 @@ public class GeofenceEngineTest extends AndroidTestCase {
     }
 
     public void testCullsStoredItemsWithBadLocations() throws IOException {
-        final PCFPushGeofenceData storedItem = ModelUtil.getJson(getContext(), "geofence_one_item_persisted_4_bad.json", new TypeToken<PCFPushGeofenceData>(){});
-        final PCFPushGeofenceDataList storedItems = new PCFPushGeofenceDataList();
-        storedItems.put(4L, storedItem);
+        final PCFPushGeofenceDataList storedItems = ModelUtil.getPCFPushGeofenceDataList(getContext(), "geofence_one_item_bad_radius.json");
         when(store.getCurrentlyRegisteredGeofences()).thenReturn(storedItems);
         engine.processResponseData(50L, new PCFPushGeofenceResponseData());
         assertRegisterGeofences(new PCFPushGeofenceLocationMap());
@@ -610,7 +605,7 @@ public class GeofenceEngineTest extends AndroidTestCase {
         verify(registrar, never()).reset();
         verify(store, never()).reset();
     }
-    
+
     public void testClearNullLocation() throws IOException {
         engine.clearLocations(null);
         verifyZeroInteractions(registrar);
